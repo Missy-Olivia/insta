c517f3064045e261197f6504b935648a36a3ab1d
Ngabire Olivia Missy <ngamissia@gmail.com>
2021-01-12 01:53:42 +0200

    added 1st view fx

--- insta/views.py
+++ insta/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.http import HttpResponse, Http404, HttpResponseRedirect
+import datetime as dt
+
 
 # Create your views here.
+def signup(request):
+    if request.user.is_authenticated():
+        return redirect('insta')
+    else:
+        if request.method == 'POST':
+            form = SignupForm(request.POST)
+            if form.is_valid():
+                user = form.save(commit=False)
+                user.save()
+                new_profile = Profile(user=user)
+        else:
+            form = SignupForm()
+    return render(request, 'registration/registration.html',{'form':form})
