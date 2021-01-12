cbb1bd72ba2069030c4b8fe28f1365402443b803
Ngabire Olivia Missy <ngamissia@gmail.com>
2021-01-12 03:48:09 +0200

    updated forms.py

---
+++ insta/forms.py
@@ -0,0 +1,17 @@
+from django import forms
+from .models import Profile, Post, Comment, Follow
+
+class AddProfileForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        exclude = ['user']
+
+class NewPostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        exclude = ['user', 'date_posted','profile','like']
+
+class NewCommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        exclude = ['post', 'user', 'date_posted']
